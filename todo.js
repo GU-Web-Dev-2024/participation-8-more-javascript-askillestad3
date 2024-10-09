44cfe805617375d820a10ae578993af5f54ea3e3
Abby Skillestad <askillestad@zagmail.gonzaga.edu>
2024-10-09 08:33:19 -0700

    function to add task

---
+++ todo.js
@@ -0,0 +1,17 @@
+// will give errors with typo mistakes, always a good practice to use, enforces good syntax
+"use strict";
+
+// function to add a task
+function addTask(task, taskList) {
+    const li = document.createElement("li");
+    li.textContent = task;
+    taskList.appendChild(li);
+}
+
+// function to clear input field
+
+
+// alternate method to export
+
+
+// exporting functions
